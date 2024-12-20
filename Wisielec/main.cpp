f8e6740c788e6373fba304d3eef35643ee6f2846
Ivan <ivan.tsvihun@gmail.com>
2024-12-20 02:48:12 +0100

    Adding function of printing string x times

--- Wisielec/main.cpp
+++ Wisielec/main.cpp
@@ -2,21 +2,34 @@
 
 using namespace std;
 
-
+void CoutSign(int amount, string sign)
+{
+	for (int i = 0; i < amount; i++)
+	{
+		//cout << endl;
+		cout << sign;
+	}
+}
 
 
 int main()
 {
 	setlocale(LC_CTYPE, "Polish");
 
+	int mistakes = 0;
+
+	cout << "This is drawing of your Hanging Man:" << endl << endl;
+
+	
+
 	//Initialize();
 		
-	while (/*!ExitGame*/ true)
-	{
-		//GetInput();
-		//UpdateGame();
-		//Render();
-	}
+	//while (/*!ExitGame*/ true)
+	//{
+	//	//GetInput();
+	//	//UpdateGame();
+	//	//Render();
+	//}
 
 	//ShutDown();
 
