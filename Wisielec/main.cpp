37b1bf2e2276b3e16793eb5d2bdc5ae7557be7e9
Ivan <ivan.tsvihun@gmail.com>
2024-12-20 02:40:05 +0100

    Base template

---
+++ Wisielec/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+
+int main()
+{
+	setlocale(LC_CTYPE, "Polish");
+
+	//Initialize();
+		
+	while (/*!ExitGame*/ true)
+	{
+		//GetInput();
+		//UpdateGame();
+		//Render();
+	}
+
+	//ShutDown();
+
+	return 0;
+}
